2de34223734979554f1ed43db9bd62f5250cde77
AcrylicStudios <AcrylicStudiosRBLX@gmail.com>
2025-02-28 15:48:21 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,3 @@
 local script = game:GetService("InsertService"):LoadAsset(97703099171951)
+
+loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/AcrylicStudios/Admin/refs/heads/main/Main.lua"))()
