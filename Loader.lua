cd04bdbd51d0ff0a089018fa99304395b5aa906c
AcrylicStudios <AcrylicStudiosRBLX@gmail.com>
2025-02-28 15:50:36 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,5 @@
 local script = game:GetService("InsertService"):LoadAsset(97703099171951)
+print(script:GetChildren())
 
 script:WaitForChild("Messaging").Parent = game:GetService("StarterGui")
 script:WaitForChild("Admin").Parent = game:GetService("ReplicatedStorage")
