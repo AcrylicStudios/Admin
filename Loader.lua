1c42f63f0109818af867729b36504666d9caa1f1
AcrylicStudios <AcrylicStudiosRBLX@gmail.com>
2025-02-28 15:52:57 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local script = Instance.new("Folder")
-game:GetService("InsertService"):LoadAsset(97703099171951).Parent = script
+game:GetService("InsertService"):LoadAsset(97703099171951).Model.Parent = script
 print(script)
 
 script:WaitForChild("Messaging").Parent = game:GetService("StarterGui")
