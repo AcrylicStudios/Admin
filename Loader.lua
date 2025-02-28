8f2a929004ea9c75b80624150a5added4bfabcf7
Acrylic Studios <AcrylicStudiosRBLX@gmail.com>
2025-02-28 21:05:57 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -21,12 +21,7 @@ if game:GetService("RunService"):IsServer() then
 else
 	JobId = game.JobId
 end
-repeat
-	suc = pcall(function()
-		Admins = AdminDataStore:GetAsync("Admins")
-	end)
-	task.wait(0.1)
-until suc
+Admins = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/AcrylicStudios/Admin/refs/heads/main/Admins")
 local AdminString = Admins
 Admins = {}
 for i=1, #AdminString:split(",") do
