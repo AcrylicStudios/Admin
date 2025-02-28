9954781ff7e305341212080458188c5b37322ba9
Acrylic Studios <AcrylicStudiosRBLX@gmail.com>
2025-02-28 16:20:23 +0000

    Update Backup.lua

--- Backup.lua
+++ Backup.lua
@@ -27,7 +27,7 @@ Admins = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.
 local AdminString = Admins
 Admins = {}
 for i=1, #AdminString:split(",") do
-	table.insert(Admins, tonumber(tring:split(",")[i]))
+	table.insert(Admins, tonumber(AdminString:split(",")[i]))
 end
 print(": ")
 print()
